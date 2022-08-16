f371917e918c9cab38aa50366f11b63eae723270
YashNahata <yashnahata16@gmail.com>
2022-08-16 23:48:08 +0530

    Initial commit

--- hultprize/settings.py
+++ hultprize/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yashnahata16@gmail.com'
-EMAIL_HOST_PASSWORD = 'aocgfffpyrnddxcx'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
