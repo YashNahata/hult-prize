e8b9aea8cdf782f2981bc55642e4bbbba11642e3
Priyanshu <pb.21u10029@btech.nitdgp.ac.in>
2022-12-07 01:45:12 +0530

    added slider in home page

---
+++ teams/static/js/swiper.js
@@ -0,0 +1,20 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let nextBtn = document.getElementById("autoClick");
+
+window.setInterval(() => {
+  nextBtn.click();
+}, 5000);
