a231606fd8f077a7812bbcc76ef1c9961a86fef9
YashNahata <yashnahata16@gmail.com>
2022-12-07 20:15:54 +0530

    Favicon added

--- hultprize/settings.py
+++ hultprize/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(b=8-nt4y)^xg(0clyfmj7&0o*(*mn#$-4ph-i59*a%p@89^5r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
